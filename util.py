df0d1dc1da4d74a4a28a591905429ace4d09f742
Nikolaus Graf <nik@deck.cc>
2012-01-12 14:32:53 +0100

    added gravatar and generate_name_from_email

--- util.py
+++ util.py
@@ -1,5 +1,7 @@
 import uuid
 import base64
+import urllib
+import hashlib
 
 def generate_key():
     """
@@ -7,3 +9,15 @@ def generate_key():
     this reduces the string size from 32 to 26 chars.
     """
     return base64.b32encode(uuid.uuid4().bytes).strip('=').lower()
+
+def gravatar(email):
+    gravatar_url = "https://secure.gravatar.com/avatar?"
+    gravatar_url += urllib.urlencode({'gravatar_id': hashlib.md5(email.encode("utf8").lower()).hexdigest(), 'd': 'https://www.blossom.io/static/img/ui/blank-avatar.gif'})
+    return gravatar_url
+
+def generate_name_from_email(email):
+    """
+    use email to generate a user name
+    by replacing dots with spaces and then titlecases words
+    """
+    return " ".join(email.split('@')[0].split('.')).title()
